ae9208b2004f31248e26fdc9317a078cf0263830
marsel <garifyllinmarsel197688@gmail.com>
2025-04-17 17:42:40 +0400

    not draw selected button

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,21 +3,28 @@ import React from 'react';
 export default class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { activeContent: null };
+    this.state = {
+      activeContent: null,
+      selectedButton: null
+     };
   }
 
   render() {
     return (
       <div>
-        {this.props.menu.map(item => (
-          <button key={item.name} onClick={() =>  this.setState({ activeContent: item.component })}>
-            {item.name}
-          </button>
+      {this.props.menu
+        .filter(item => item.name !== this.state.selectedButton)
+        .map(item => (
+        <button
+          key={item.name}
+          onClick={() => this.setState({ activeContent: item.component, selectedButton: item.name })}
+        >
+          {item.name}
+        </button>
         ))}
-
-        <div>
-          {this.state.activeContent}
-        </div>
+      <div>
+        {this.state.activeContent}
+      </div>
       </div>
     );
   }
