f2df36bb3900a938b2a236dcb0e65f7abf4fef7b
marsel <garifyllinmarsel197688@gmail.com>
2025-05-15 18:10:22 +0400

    Refactor: rewrite menu to functional component

---
+++ src/modules/graph3d/Math3D/figurs/Torus.js
@@ -0,0 +1,116 @@
+import Point from '../entities/Point.js';
+import Polygon from '../entities/Polygon.js';
+import Edge from '../entities/Edge.js';
+import Figure from '../entities/Figure.js';
+
+class Torus extends Figure {
+    constructor(radialSegments = 20, tubeSegments = 10, radius = 10, tubeRadius = 3) {
+        super();
+        this.radialSegments = radialSegments;
+        this.tubeSegments = tubeSegments;
+        this.radius = radius;
+        this.tubeRadius = tubeRadius;
+        this.points = [];
+        this.edges = [];
+        this.polygons = [];
+        this.update();
+    }
+
+    update() {
+        this.points = [];
+        this.edges = [];
+        this.polygons = [];
+        this.generatePoints();
+        this.generateEdges();
+        this.generatePolygons();
+    }
+
+    generatePoints() {
+        const radialStep = (2 * Math.PI) / this.radialSegments;
+        const tubeStep = (2 * Math.PI) / this.tubeSegments;
+
+        for (let r = 0; r < this.radialSegments; r++) {
+            const radialAngle = r * radialStep;
+            const cosRadial = Math.cos(radialAngle);
+            const sinRadial = Math.sin(radialAngle);
+            
+            for (let t = 0; t < this.tubeSegments; t++) {
+                const tubeAngle = t * tubeStep;
+                const cosTube = Math.cos(tubeAngle);
+                const sinTube = Math.sin(tubeAngle);
+                
+                this.points.push(new Point(
+                    (this.radius + this.tubeRadius * cosTube) * cosRadial,
+                    (this.radius + this.tubeRadius * cosTube) * sinRadial,
+                    this.tubeRadius * sinTube
+                ));
+            }
+        }
+    }
+
+    generateEdges() {
+        for (let r = 0; r < this.radialSegments; r++) {
+            const ringStart = r * this.tubeSegments;
+            for (let t = 0; t < this.tubeSegments; t++) {
+                this.edges.push(new Edge(
+                    ringStart + t,
+                    ringStart + (t + 1) % this.tubeSegments
+                ));
+            }
+        }
+
+        for (let t = 0; t < this.tubeSegments; t++) {
+            for (let r = 0; r < this.radialSegments; r++) {
+                this.edges.push(new Edge(
+                    r * this.tubeSegments + t,
+                    ((r + 1) % this.radialSegments) * this.tubeSegments + t
+                ));
+            }
+        }
+    }
+
+    generatePolygons() {
+        for (let r = 0; r < this.radialSegments; r++) {
+            for (let t = 0; t < this.tubeSegments; t++) {
+                const a = r * this.tubeSegments + t;
+                const b = r * this.tubeSegments + (t + 1) % this.tubeSegments;
+                const c = ((r + 1) % this.radialSegments) * this.tubeSegments + (t + 1) % this.tubeSegments;
+                const d = ((r + 1) % this.radialSegments) * this.tubeSegments + t;
+                
+                this.polygons.push(new Polygon([a, b, c, d]));
+            }
+        }
+    }
+
+    settings() {
+        return (
+            <div>
+                <label>
+                    Радиус:
+                    <input
+                        type="number"
+                        defaultValue={this.radius}
+                        onChange={(e) => {
+                            this.radius = parseFloat(e.target.value);
+                            this.update();
+                        }}
+                    />
+                </label>
+                <label>
+                    Количество полигонов:
+                    <input
+                        type="number"
+                        defaultValue={this.radialSegments}
+                        onChange={(e) => {
+                            this.radialSegments = parseInt(e.target.value, 10);
+                            this.update();
+                        }}
+                    />
+                </label>
+            </div>
+        );
+    }
+}
+
+
+export default Torus;
