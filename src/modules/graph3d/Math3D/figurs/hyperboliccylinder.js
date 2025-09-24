4b03ec2a5eb5189755c5b936e1a40a10ee9ee3e8
marsel <garifyllinmarsel197688@gmail.com>
2025-09-24 07:31:13 +0400

    fix

--- src/modules/graph3d/Math3D/figurs/hyperboliccylinder.js
+++ src/modules/graph3d/Math3D/figurs/hyperboliccylinder.js
@@ -4,7 +4,7 @@ import Point from '../entities/Point.js';
 import Polygon from '../entities/Polygon.js';
 
 export default class HyperbolicCylinder extends Figure {
-    constructor(origin = new Point(0, 0, 0), count = 10) {
+    constructor(origin = new Point(0, 0, 0), count = 25) {
         super(origin);
         this.count = count;
         this.points = [];
@@ -14,74 +14,66 @@ export default class HyperbolicCylinder extends Figure {
     }
 
     updateGeometry() {
-        let size = 5;
+        const size = 5;
+        const count = this.count;
         this.points = [];
         this.edges = [];
         this.polygons = [];
-        for (let i = -this.count; i < this.count; i++) {
-            for (let j = 0; j < this.count; j++) {
-                const x = i + size / this.count;
-                const y = x * x / size;
+
+        for (let i = -count; i < count; i++) {
+            for (let j = 0; j < count; j++) {
+                const x = i;
+                const y = (x * x) / size;
                 const z = j - size;
                 this.points.push(new Point(x, y, z));
             }
         }
-        size = -5;
-        for (let i = -this.count; i < this.count; i++) {
-            for (let j = 0; j < this.count; j++) {
-                const x = i - size / this.count;
-                const y = x * x / size;
-                const z = j + size;
+
+        for (let i = -count; i < count; i++) {
+            for (let j = 0; j < count; j++) {
+                const x = i;
+                const y = -(x * x) / size;
+                const z = j - size;
                 this.points.push(new Point(x, y, z));
             }
         }
-        for (let i = 0; i < this.points.length / 2 - this.count; i++) {
-            if (i + 1 < this.points.length && (i + 1) % this.count !== 0) {
+
+        const half = this.points.length / 2;
+
+        for (let i = 0; i < half - count; i++) {
+            const isRightEdge = (i + 1) % count === 0;
+            if (!isRightEdge) {
                 this.edges.push(new Edge(i, i + 1));
-            } else if ((i + 1) % this.count === 0) {
-                this.edges.push(new Edge(i, i + 1 - this.count));
+                this.polygons.push(new Polygon([i, i + 1, i + 1 + count, i + count]));
             }
-            if (i < this.points.length - this.count) {
-                this.edges.push(new Edge(i, i + this.count));
-            }
+            this.edges.push(new Edge(i, i + count));
         }
-        for (let i = this.points.length / 2; i < this.points.length; i++) {
-            if (i + 1 < this.points.length && (i + 1) % this.count !== 0) {
+
+        for (let i = half; i < this.points.length - count; i++) {
+            const isRightEdge = (i + 1) % count === 0;
+            if (!isRightEdge) {
                 this.edges.push(new Edge(i, i + 1));
-            } else if ((i + 1) % this.count === 0) {
-                this.edges.push(new Edge(i, i + 1 - this.count));
+                this.polygons.push(new Polygon([i, i + 1, i + 1 + count, i + count]));
             }
-            if (i < this.points.length - this.count) {
-                this.edges.push(new Edge(i, i + this.count));
-            }
+            this.edges.push(new Edge(i, i + count));
         }
-        for (let i = 0; i < this.points.length / 2 - this.count; i++) {
-            if (i + 1 + this.count < this.points.length && (i + 1) % this.count !== 0) {
-                this.polygons.push(new Polygon([i, i + 1, i + 1 + this.count, i + this.count]));
-            } else if (i + this.count < this.points.length && (i + 1) % this.count === 0) {
-                this.polygons.push(new Polygon([i, i + 1 - this.count, i + 1, i + this.count]));
-            }
-        }
-        for (let i = this.points.length / 2 + this.count / 2; i < this.points.length; i++) {
-            if (i + 1 + this.count < this.points.length && (i + 1) % this.count !== 0) {
-                this.polygons.push(new Polygon([i, i + 1, i + 1 + this.count, i + this.count]));
-            } else if (i + this.count < this.points.length && (i + 1) % this.count === 0) {
-                this.polygons.push(new Polygon([i, i + 1 - this.count, i + 1, i + this.count]));
-            }
-        }
+
         this.setIndexPolygons();
     }
 
     settings() {
         return (
             <div>
-                <label>
+                <label> 
                     Количество полигонов:
                     <input
                         type="number"
                         defaultValue={this.count}
                         min={3}
-                        onChange={e => { this.count = parseInt(e.target.value, 10) || 3; this.updateGeometry(); }}
+                        onChange={e => {
+                            this.count = parseInt(e.target.value, 10) || 3;
+                            this.updateGeometry();
+                        }}
                     />
                 </label>
             </div>
